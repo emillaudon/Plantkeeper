1e601f70e0df21ec8621284db3fabe795016d402
emil <emil.laudn@gmail.com>
2021-05-20 18:20:36 +0200

    fixed images from camera not working on create plant

--- app/src/main/java/com/example/plantkeeper/fragments/CreateFragment.kt
+++ app/src/main/java/com/example/plantkeeper/fragments/CreateFragment.kt
@@ -3,15 +3,14 @@ package com.example.plantkeeper.fragments
 import android.Manifest
 import android.app.Activity
 import android.app.Dialog
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
@@ -19,13 +18,20 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.example.plantkeeper.BuildConfig
 import com.example.plantkeeper.R
 import com.example.plantkeeper.models.NetworkHandler
 import com.example.plantkeeper.models.Plant
 import com.example.plantkeeper.models.PlantUpdate
 import kotlinx.android.synthetic.main.fragment_create.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -47,6 +53,7 @@ private val PERMISSIONS_STORAGE = arrayOf(
  */
 class CreateFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    val CAMERA_PERM_CODE: Int = 101
     private var RESULT_LOAD_IMAGE: Int = 1
     val REQUEST_IMAGE_CAPTURE = 1
 
@@ -64,6 +71,8 @@ class CreateFragment : Fragment() {
     lateinit var heightTextView: TextView
 
     lateinit var noteEditText: EditText
+
+    lateinit var currentPhotoPath: String
 
     var height: Double = -10.0
 
@@ -104,15 +113,7 @@ class CreateFragment : Fragment() {
 
         image.setOnClickListener {
             verifyStoragePermissions(activity)
-            /*
-            val i = Intent(
-                Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-            )
-            startActivityForResult(i, RESULT_LOAD_IMAGE)
-
-             */
-
-            dispatchTakePictureIntent()
+            askCameraPermissions()
         }
 
         heightTextView.setOnClickListener {
@@ -131,10 +132,15 @@ class CreateFragment : Fragment() {
             var listOfUpdates = mutableListOf<PlantUpdate>()
             listOfUpdates.add(PlantUpdate(height, "placeHolder", "placeHolder", 100))
 
+            imgPath = currentPhotoPath
+
             val newPost = Plant(imgPath, text, watering, temperature, sunlight, noteEditText.text.toString(), height, "placeHolder", listOfUpdates, 1)
             val handler = NetworkHandler()
 
             handler.newPlant(newPost, imgPath)
+
+            println(imgPath)
+            println("number two")
 
             val newFragment: Fragment = HomeFragment()
             val transaction: FragmentTransaction = requireFragmentManager().beginTransaction()
@@ -148,34 +154,55 @@ class CreateFragment : Fragment() {
         return rootView
     }
 
-
-
     private fun dispatchTakePictureIntent() {
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
+
+            var photoFile: File? = null
+            try {
+                photoFile = createImageFile()
+            } catch (ex: IOException) {
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    requireContext(),
+                    BuildConfig.APPLICATION_ID +".fileprovider",
+                    photoFile
+                )
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+
+    }
+
+    private fun askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.CAMERA
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                CAMERA_PERM_CODE
+            )
+        } else {
+            dispatchTakePictureIntent()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && data != null) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            val f = File(currentPhotoPath)
 
-            val projection =
-                arrayOf(MediaStore.Images.Media.DATA)
-            val cursor: Cursor = requireActivity().managedQuery(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                projection, null, null, null
-            )
-            val column_index_data: Int = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            cursor.moveToLast()
-
-            imgPath = cursor.getString(column_index_data)
-            val bitmapImage = BitmapFactory.decodeFile(imgPath)
-            image.setImageBitmap(bitmapImage)
+            image.setImageURI(Uri.fromFile(f))
+            println("4")
             image.scaleType = ImageView.ScaleType.CENTER_CROP
+
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            val contentUri = Uri.fromFile(f)
+            mediaScanIntent.data = contentUri
+            requireContext().sendBroadcast(mediaScanIntent)
         } else if (data != null) {
             /// Code for using gallery for later
             val selectedImage: Uri? = data.data
@@ -199,6 +226,22 @@ class CreateFragment : Fragment() {
             Toast.makeText(activity, "Try Again!!", Toast.LENGTH_SHORT).show()
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun createImageFile(): File? {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+
+        val storageDir: File =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val image: File = File.createTempFile(
+            imageFileName,
+            ".jpg",
+            storageDir
+        )
+
+        currentPhotoPath = image.getAbsolutePath()
+        return image
     }
 
     fun showDialogFor(bar: ProgressBar) {
