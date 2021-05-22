31c0235be99c2132bb0a2b027996b51123ee43d9
emil <emil.laudn@gmail.com>
2021-05-22 20:03:48 +0200

    added unit test for bubble sorting

--- app/src/test/java/com/example/plantkeeper/ExampleUnitTest.kt
+++ app/src/test/java/com/example/plantkeeper/ExampleUnitTest.kt
@@ -1,17 +1,29 @@
 package com.example.plantkeeper
 
+import com.example.plantkeeper.models.Plant
+import com.example.plantkeeper.models.PlantSorter
+import com.example.plantkeeper.models.PlantSorter.Companion.bubbleSort
+import com.example.plantkeeper.models.PlantUpdate
 import org.junit.Test
 
 import org.junit.Assert.*
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
 class ExampleUnitTest {
+
+    ///Tests if plants are sorted correctly according to creationtime, the plant with the highest creationtime should be first.
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun plantsAreSorted() {
+        var updatesList = listOf<PlantUpdate>()
+        var plantOne = Plant("1", "1", 1, 1, 1, "1", 1.1, "", updatesList, 1)
+        var plantTwo = Plant("1", "1", 1, 1, 1, "1", 1.1, "", updatesList, 2)
+        var plantThree = Plant("1", "1", 1, 1, 1, "1", 1.1, "", updatesList, 3)
+        var plantFour = Plant("1", "1", 1, 1, 1, "1", 1.1, "", updatesList, 4)
+
+        var plantArray = arrayOf(plantTwo, plantFour, plantOne, plantThree)
+
+        plantArray = plantArray.bubbleSort()
+
+        assertEquals(plantFour.creationTime, plantArray[0].creationTime)
     }
 }
